5dc0fb8ec1172198f3c0b4a3b2fd065393d7128e
Tsur <prucheta@gmail.com>
2016-07-29 22:06:21 +0200

    updated demo site to 0.0.2 :mouse:

--- examples/home/index.js
+++ examples/home/index.js
@@ -2,6 +2,38 @@
 import BigMouse from 'bigmouse';
 import BigMouseParticles from 'bigmouse-particles';
 
+const randColor1 = () => {
+
+    const colors = ["red"];
+
+    return colors[~~(Math.random()*colors.length)];
+};
+
+const randColor2 = () => {
+
+    const colors = ["rgb(244, 98, 127)", "rgb(244, 22, 147)", "rgb(244, 144, 167)", "rgb(110, 215, 197)", "rgb(244, 215, 217)"];
+
+    return colors[~~(Math.random()*colors.length)];
+};
+
+
+const randColor3 = () => {
+
+    const colors = ["red", "pink"];
+
+    return colors[~~(Math.random()*colors.length)];
+};
+
+
+const randColor4 = () => {
+
+    const colors = [ "orange", "purple", "orange", "purple", "orange", "red", "purple", "red"];
+
+    return colors[~~(Math.random()*colors.length)];
+};
+
+
+
 BigMouse.cheese({
 
     // Ask for cheeses you love
@@ -14,19 +46,19 @@ BigMouse.cheese({
         switch(mouse.rel){
 
             case "example.blogspot.com/1":
-                cheese('particles', {num:3, size:25, color:"hsla(0, 10%, 50%, 1)"});
+                cheese('particles', {num:3, size:10, color: randColor1});
                 break;
 
             case "example.blogspot.com/2":
-                cheese('particles', {num:70, size:5, color:"hsla(0, 80%, 50%, 1)"});
+                cheese('particles', {num:70, size:10, color: randColor2});
                 break;
 
             case "example.blogspot.com/3":
-                cheese('particles', {num:10, size:1, color:"hsla(0, 50%, 50%, 1)"});
+                cheese('particles', {num:10, size:10, color: randColor3});
                 break;
 
             case "example.blogspot.com/4":
-                cheese('particles', {num:30, size:10, color:"hsla(0, 80%, 50%, 1)", radius: 50});
+                cheese('particles', {num:30, size:10, color: randColor4});
                 break;
 
             default: cheese();
